e9f5e3549a92e1c50ec6195d36df91f0e2b9bee3
Folasade Olayanju <aanuoluwanio@gmail.com>
2022-06-07 20:01:40 +0100

    First Commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// complete the function
+function prime(num) {
+    // code goes here
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0) {
+            return `${num} is not a prime number`
+        } else {
+            return `${num} is a prime number`
+        }
+    }
+}
+
+function solution(arg) {
+    return prime(arg);
+}
+
+if (typeof require !== "undefined" && require.main === module) {
+    if (process.argv.length <= 2) {
+        throw Error("Require argument");
+    }
+
+    const arg = process.argv[2];
+    console.log(solution(arg));
+}
+
+module.exports = { solution };
